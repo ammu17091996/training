71fb064fd79caf23b2f61c3815eab48f3ac6402d
nithya.nair <nithya.nair@SPTVMLAP-5125.speridian.com>
2025-03-04 17:07:12 +0530

    programs

---
+++ training/src/main/java/training/Amstrong.java
@@ -0,0 +1,34 @@
+package training;
+
+import java.util.Scanner;
+
+public class Amstrong {
+
+	public static void main(String[] args) {
+		int a,remainder,sum=0;
+		String st;
+		System.out.print("enter the number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		int exp=a;
+		int number=a;
+st=Integer.toString(a);
+int length=st.length();
+
+while(number>0)
+{
+remainder=number%10;
+int mul=1;
+for(int i=1;i<=length;i++)
+{
+mul=mul*remainder;	
+}
+sum=sum+mul;
+number=number/10;
+
+}
+if(sum==exp)
+	System.out.print("armstrong");
+	}
+
+}
